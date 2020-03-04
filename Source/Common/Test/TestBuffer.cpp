a98bb4732dbb24ff86e3296c1fe3713e911dafa0
ankarako <ankarako@iti.gr>
2020-03-04 23:08:14 +0200

    + buffercopy

--- Source/Common/Test/TestBuffer.cpp
+++ Source/Common/Test/TestBuffer.cpp
@@ -33,4 +33,30 @@ TEST_CASE("Common", "Buffer")
 		REQUIRE(myBuffer->ByteSize() == kBufferElmntCount * sizeof(int));
 		REQUIRE(myBuffer->Data() != nullptr);
 	};
+	SECTION("Buffer: CPU->CPU", "Copy")
+	{
+		static constexpr size_t	kBufferElmntCount = 16;
+		fu::BufferCPU<int> srcBuffer = fu::CreateBufferCPU<int>(kBufferElmntCount);
+		fu::BufferCPU<int> dstBuffer = fu::CreateBufferCPU<int>(kBufferElmntCount);
+		for (int i = 0; i < kBufferElmntCount; i++)
+		{
+			srcBuffer->Data()[i] = i;
+		}
+		fu::BufferCopy(dstBuffer, srcBuffer);
+		for (int i = 0; i < kBufferElmntCount; i++)
+		{
+			REQUIRE(dstBuffer->Data()[i] == i);
+		}
+	};	///	!SECTION("Buffer: CPU->CPU", "Copy")
+	SECTION("Buffer: CPU->GPU", "Copy")
+	{
+		static constexpr size_t	kBufferElmntCount = 16;
+		fu::BufferCPU<int> srcBuffer = fu::CreateBufferCPU<int>(kBufferElmntCount);
+		fu::BufferGPU<int> dstBuffer = fu::CreateBufferGPU<int>(kBufferElmntCount);
+		for (int i = 0; i < kBufferElmntCount; i++)
+		{
+			srcBuffer->Data()[i] = i;
+		}
+		fu::BufferCopy(dstBuffer, srcBuffer);
+	};	///	!SECTION("Buffer: CPU->GPU", "Copy")
 };	///	!TEST_CASE("Common", "Buffer")
