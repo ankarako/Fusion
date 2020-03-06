9ab4db80b6220c99c2b5fed0653e0a46eacfc961
ankarako <ankarako@iti.gr>
2020-03-06 19:29:10 +0200

    -deleted cpp-taskflow -deleted video-pp -deleted entt
    Signed-off-by: ankarako <ankarako@iti.gr>

--- 3rdParty/entt/test/entt/core/type_info.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-#include <entt/core/hashed_string.hpp>
-#include <entt/core/type_info.hpp>
-
-TEST(TypeId, Functionalities) {
-    ASSERT_NE(entt::type_info<int>::id(), entt::type_info<const int>::id());
-    ASSERT_NE(entt::type_info<int>::id(), entt::type_info<char>::id());
-    ASSERT_EQ(entt::type_info<int>::id(), entt::type_info<int>::id());
-}
