c12402859d8ebdfbdb2742368166b7f2beabfb2a
ankarako <ankarako@iti.gr>
2020-03-09 23:29:48 +0200

    - really close to rendering video
    Signed-off-by: ankarako <ankarako@iti.gr>

--- Source/OptixRayTracer/Private/Programs/EnvMapMiss.cu
+++ Source/OptixRayTracer/Private/Programs/EnvMapMiss.cu
@@ -8,18 +8,18 @@ rtDeclareVariable(uint2, launch_index, rtLaunchIndex, );
 ///=======================================
 ///	Miss program for 360 environment maps
 ///=======================================
-rtTextureSampler<uchar4, 2> EnvMapTexSampler;
+rtTextureSampler<float4, 2> EnvMapTexSampler;
 RT_PROGRAM void EnvMapMiss()
 {
 	unsigned int idx = launch_index.x + launch_index.y;
-	if (idx == 0)
-	{
-		printf("current launch idx: %u\n", idx);
-	}
 	float theta = atan2f(ray.direction.x, ray.direction.z);
 	float phi	= M_PIf * 0.5f - acosf(ray.direction.y);
 	float u		= (theta + M_PIf) * (0.5f * M_1_PIf);
 	float v		= 0.5f * (1.0f + sinf(phi));
-	
-	prd_radiance.Result = tex2D(EnvMapTexSampler, u, v);
+	float4 color = tex2D(EnvMapTexSampler, u, v);
+	// if (idx == 0)
+	// 	printf("color: (%f, %f, %f, %f)\n", color.x, color.y, color.x, color.w);
+	prd_radiance.Result = fu::rt::make_color(optix::make_float3(color));
+	// if (idx == 0)
+	// 	printf("color: (%u, %u, %u, %u)\n", prd_radiance.Result.x, prd_radiance.Result.y, prd_radiance.Result.x, prd_radiance.Result.w);
 }
--- Source/OptixRayTracer/Private/Programs/EnvmapRaygen.cu
+++ Source/OptixRayTracer/Private/Programs/EnvmapRaygen.cu
@@ -19,7 +19,7 @@ RT_PROGRAM void EnvMapRaygen()
 {
 	optix::size_t2 screen_size = output_buffer.size();
 	optix::float2 d = optix::make_float2(launch_index) / optix::make_float2(screen_size) * optix::make_float2(2.0f * M_PIf, M_PIf) + optix::make_float2(M_PIf, 0);
-	optix::float3 angle = optix::make_float3(cos(d.x) * sin(d.y), -cos(d.y), sin(d.y) * sin(d.y));
+	optix::float3 angle = optix::make_float3(cos(d.x) * sin(d.y), -cos(d.y), sin(d.x) * sin(d.y));
 	optix::float3 ray_origin = eye;
 	optix::float3 ray_direction = optix::normalize(angle.x * optix::normalize(U) + angle.y * optix::normalize(V) + angle.z * optix::normalize(W));
 	optix::Ray ray = optix::make_Ray(ray_origin, ray_direction, radiance_ray_type, scene_epsilon, RT_DEFAULT_MAX);
@@ -29,6 +29,5 @@ RT_PROGRAM void EnvMapRaygen()
 	prd.Depth = 0;
 
 	rtTrace(top_object, ray, prd);
-
 	output_buffer[launch_index] = prd.Result;
 }
