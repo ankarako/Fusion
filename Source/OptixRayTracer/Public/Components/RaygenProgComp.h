37f0d689f742c2f07a049c825f66627960d3fc47
perukas <ankarakottas@gmail.com>
2020-03-10 15:26:09 +0200

    we have video playback, slow

--- Source/OptixRayTracer/Public/Components/RaygenProgComp.h
+++ Source/OptixRayTracer/Public/Components/RaygenProgComp.h
@@ -1,10 +1,7 @@
 #ifndef __OPTIXRAYTRACER_PUBLIC_COMPONENTS_RAYGENPROGCOMP_H__
 #define __OPTIXRAYTRACER_PUBLIC_COMPONENTS_RAYGENPROGCOMP_H__
 
-#include <optix.h>
-#include <optixu/optixpp_namespace.h>
-#include <optixu/optixu_vector_types.h>
-#include <optixu/optixu_vector_functions.h>
+#include <optix_world.h>
 #include <memory>
 
 namespace fu {
@@ -13,10 +10,11 @@ namespace rt {
 ///	\brief an optix program component for pinhole ray generation
 struct RaygenProgCompObj
 {
-	optix::Program	RaygenProg;
-	optix::float3	Eye{ 0.0f, 0.0f, 0.0f };
-	optix::float3	Up{ 0.0f, 0.0f, 0.0f };
-	optix::float3	Lookat{ 0.0f, 0.0f, 0.0f };
+	optix::Program		RaygenProg;
+	optix::float3		Eye{ 0.0f, 0.0f, 0.0f };
+	optix::float3		Up{ 0.0f, 0.0f, 0.0f };
+	optix::float3		Lookat{ 0.0f, 0.0f, 0.0f };
+	optix::Matrix4x4	Transform;
 	int		ViewWidth{ 0 };
 	int		ViewHeight{ 0 };
 	float	AspectRatio{ 0.0f };
