2c4720e0493bee62411b84957130660447e56c52
perukas <ankarakottas@gmail.com>
2020-05-14 17:18:44 +0300

    deleted non used files added blosc in 3rdParty
    Signed-off-by: perukas <ankarakottas@gmail.com>

--- Source/Application/Public/FontDef.h
+++
@@ -1,8 +0,0 @@
-#ifndef __APPLICATION_PUBLIC_FONTDEF_H__
-#define __APPLICATION_PUBLIC_FONTDEF_H__
-
-#include <IconsFontAwesome5.h>
-#include <IconsMaterialDesign.h>
-#include <IconsKenney.h>
-
-#endif	///	!__APPLICATION_PUBLIC_FONTDEF_H__
